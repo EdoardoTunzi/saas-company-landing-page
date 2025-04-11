5b738f9a67391a4e41d341aaa06b31796600e428
Edoardo <edfl.tunzi@gmail.com>
2025-04-11 17:30:05 +0200

    new icons in services section

--- src/utils/services-data.tsx
+++ src/utils/services-data.tsx
@@ -1,29 +1,21 @@
+import { LuBrain } from "react-icons/lu";
+import { MdOutlineInsights } from "react-icons/md";
+import { VscGraph } from "react-icons/vsc";
+
 export const services = [
   {
     title: "Intelligent Automation",
     description: "Streamline processes and reduce manual tasks.",
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-      </svg>
-    )
+    icon: <LuBrain size={25} />
   },
   {
     title: "Real‑Time Analytics",
     description: "Gain actionable insights as events unfold.",
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-      </svg>
-    )
+    icon: <VscGraph size={25} />
   },
   {
     title: "Predictive Insights",
     description: " Anticipate trends and mitigate risks before they occur.",
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-      </svg>
-    )
+    icon: <MdOutlineInsights size={25} />
   }
 ];
